a8fdf506b546ce6194207804b37e697abc34fbb9
Tai Phan <tai101568@gmail.com>
2016-02-23 09:57:01 +0700

    removed old libs

--- sensolus/models/historical_data.js
+++
@@ -1,6 +0,0 @@
-var HistoricalDataAPI = {
-        
-    getHistoricalFeedData: function(nodeID,outputRefName, f, fe) {
-    	$.getJSON('/server/rest/historicaldata/' + nodeID+"/"+outputRefName + "/all", f).error(fe);
-    }
-}
